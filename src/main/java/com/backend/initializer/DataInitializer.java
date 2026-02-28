cd15a37d36d5f644f243aaa567e321095467c468
Bibek Sah <sdevil78088@gmail.com>
2026-02-28 22:22:58 +0545

    render deployment

--- src/main/java/com/backend/initializer/DataInitializer.java
+++ src/main/java/com/backend/initializer/DataInitializer.java
@@ -1,26 +1,30 @@
-//package com.backend.initializer;
-//
-//import com.backend.Entity.Role;
-//import com.backend.Enum.UserRole;
-//import com.backend.Repository.RoleRepository;
-//import jakarta.annotation.PostConstruct;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DataInitializer {
-//
-//    private final RoleRepository roleRepository;
-//
-//    @PostConstruct
-//    public void init() {
-//
-//        if (roleRepository.count() == 0) {
-//            roleRepository.save(new Role(null, UserRole.ADMIN));
-//            roleRepository.save(new Role(null, UserRole.USER));
-//            roleRepository.save(new Role(null, UserRole.GUEST));
-//            roleRepository.save(new Role(null, UserRole.STAFF));
-//        }
-//    }
-//}
+package com.backend.initializer;
+
+import com.backend.Entity.Role;
+import com.backend.Enum.UserRole;
+import com.backend.Repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer {
+
+    private final RoleRepository roleRepository;
+
+    @PostConstruct
+    public void init() {
+        for (UserRole roleEnum : UserRole.values()) {
+            roleRepository.findByName(roleEnum).orElseGet(() -> {
+                Role role = new Role();
+                role.setName(roleEnum);
+                Role savedRole = roleRepository.save(role);
+                log.info("Role initialized: {}", savedRole.getName());
+                return savedRole;
+            });
+        }
+    }
+}
